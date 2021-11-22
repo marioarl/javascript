3308b6db7ed1d39f94affe80f8dee1db90f765cb
Mario Augusto Rego LIma <supermario_3@live.com>
2021-11-22 12:14:31 -0300

    criacao do script.js

---
+++ Aula9/script.js
@@ -0,0 +1,7 @@
+var p1 = window.document.getElementsByTagName('p')[0]
+        window.document.write(p1.innerText)
+        /*var d = window.document.getElementById('msg')
+        d.style.background = 'green'
+        */
+        var d = window.document.querySelector('div#msg')
+        d.style.background = 'grey'
