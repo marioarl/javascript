54f61bbd24c9cc5b2fa178b3668bebcceaa84a35
Mario Augusto Rego LIma <supermario_3@live.com>
2021-11-22 12:11:21 -0300

    criacao do script3.js

---
+++ Aula6/script3.js
@@ -0,0 +1,4 @@
+var nome = window.prompt('Qual é o seu nome?');
+        document.write(`Olá ${nome}. Seu nome tem ${nome.length} letras <br>`);
+        document.write(`Seu nome em MAIUSCULAS é ${nome.toUpperCase()} <br>`);
+        document.write(`Seu nome em minusculo é ${nome.toLowerCase()}`)
