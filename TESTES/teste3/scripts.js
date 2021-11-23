4fe0aee0eab905c75580fbcff3120969fb5ef299
Mario Augusto Rego LIma <supermario_3@live.com>
2021-11-23 10:54:01 -0300

    alteracao no teste2 e criacao do teste3

---
+++ TESTES/teste3/scripts.js
@@ -0,0 +1,69 @@
+let num = document.querySelector('input#fnum')
+let lista = document.querySelector('select#flista')
+let res = document.querySelector('div#res')
+let valores = []
+
+function isNumero(n) {
+    if (Number(n) >= 1 && Number(n) <=100) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function inLista(n, l){
+    if (l.indexOf(Number(n)) != -1) {
+        return true
+    } else {
+        return false
+    }
+
+
+}
+function adicionar() {
+    if (isNumero(num.value) && !inLista(num.value, valores)) {
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado.`
+        lista.appendChild(item)
+        res.innerHTML = ""
+    } else if (inLista(num.value, valores)){
+        window.alert("Valor já está na lista!!")
+        } else if (!isNumero(num.value)) {
+            window.alert("Valor inválido")
+    }
+    num.value = ''
+    num.focus()
+}
+
+function finalizar() {
+    if (valores.length == 0) {
+        window.alert('Adicione valores antes de finalizar')
+    } else {
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0 
+        let media = 0
+        for (let pos in valores) {
+            soma += valores[pos]
+            if (valores[pos] > maior)
+                maior = valores[pos]
+            if (valores[pos] < menor)
+                menor = valores[menor]    
+        }
+        media = soma / tot
+
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo temos ${tot} numeros cadastrados</p>`
+        res.innerHTML += `<p>O maior valor informado foi ${maior}.</p>`
+        res.innerHTML += `<p>O menor valor informado foi ${menor}.</p>`
+        res.innerHTML += `<p>Somando todos os valores temos ${soma}.</p>`
+        res.innerHTML += `<p>A media dos valores digitados é ${media}</p>`
+    }
+}
+
+function refresh() {
+    document.location.reload(true)
+
+}
